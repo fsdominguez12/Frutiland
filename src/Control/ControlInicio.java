d3a87111ef5c4a1b4e1587af2c23d47fc9c9f030
Dominguez <Dominguez@Dominguez>
2021-04-05 08:27:22 -0500

    avance1

---
+++ src/Control/ControlInicio.java
@@ -0,0 +1,32 @@
+
+package Control;
+
+import Interfaces.Vista_Inicio;
+import Interfaces.Vista_Principal;
+
+public class ControlInicio {
+    
+Vista_Inicio vi;
+
+    public ControlInicio(Vista_Inicio vi) {
+        this.vi = vi;
+        vi.setVisible(true);
+        vi.setLocationRelativeTo(null);
+    }
+    
+     public void IniciaControl(){
+         vi.getBtnUsuario1().addActionListener(l->Usuario1());
+         vi.getBtnUsuario2().addActionListener(l->Usuario2());
+    }
+    
+     public void Usuario1(){
+        Vista_Principal vista = new Vista_Principal();
+        ControlVentanaPrincipal cvp = new ControlVentanaPrincipal(vista);
+        cvp.IniciaControl();
+     }
+     
+     public void Usuario2(){
+        
+     }
+     
+}
