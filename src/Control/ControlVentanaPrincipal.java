3870f5fc470c2cc4537dfea0d2a3228ba6e711a3
Dominguez <Dominguez@Dominguez>
2021-04-05 14:35:32 -0500

    avance6

--- src/Control/ControlVentanaPrincipal.java
+++ src/Control/ControlVentanaPrincipal.java
@@ -6,15 +6,16 @@
 package Control;
 
 import Interfaces.Vista_Cliente;
+import Interfaces.Vista_Ensalada;
 import Interfaces.Vista_Ingrediente;
+import Interfaces.Vista_Inicio;
 import Interfaces.Vista_Principal;
 import Modelo.ModeloCliente;
+import Modelo.ModeloEnsalada;
 import Modelo.ModeloIngrediente;
+import javax.swing.JFrame;
 
-/**
- *
- * @author Jonnathan
- */
+
 public class ControlVentanaPrincipal {
     
     Vista_Principal vp;
@@ -23,17 +24,21 @@ public class ControlVentanaPrincipal {
         this.vp = vp;
         vp.setVisible(true);
         vp.setLocationRelativeTo(null);
+        vp.setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
     
     public void IniciaControl(){
         vp.getBtncliente().addActionListener(l->Cliente());
         vp.getBtnsalir().addActionListener(l->Salir());
         vp.getBtningrediente().addActionListener(l->Ingrediente());
+        vp.getBtnensalada().addActionListener(l->Ensalada());
+       
     }
     
     private void Cliente(){
         ModeloCliente modelo = new ModeloCliente();
         Vista_Cliente vista = new Vista_Cliente();
+        vp.getDesktop().add(vista);
         ControlCliente cli = new ControlCliente(modelo, vista);
         cli.IniciaControl();
     }
@@ -41,9 +46,19 @@ public class ControlVentanaPrincipal {
      private void Ingrediente(){
         ModeloIngrediente modeloIng = new ModeloIngrediente();
         Vista_Ingrediente vistaIng = new Vista_Ingrediente();
+        vp.getDesktop().add(vistaIng);
         ControlIngrediente ingr=new ControlIngrediente(modeloIng,vistaIng);
         ingr.IniciaControl();
+        ingr.lis();
     }
+     
+     private void Ensalada(){
+        ModeloEnsalada modeloEns = new ModeloEnsalada();
+        Vista_Ensalada vistaEns = new Vista_Ensalada();
+        vp.getDesktop().add(vistaEns);
+        ControlEnsalada ens=new ControlEnsalada(modeloEns,vistaEns);
+        ens.IniciaControl();
+     }
     
     private void Salir(){
         System.exit(0);
